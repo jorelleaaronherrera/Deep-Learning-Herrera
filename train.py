27afb27612884228c74aa07116bc8d3818c08495
jorelleaaronherrera <79895576+jorelleaaronherrera@users.noreply.github.com>
2022-05-03 07:49:57 +0800

    Update train.py

--- train.py
+++ train.py
@@ -82,7 +82,7 @@ if __name__== "__main__" :
 
     # construct an optimizer
     params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(params, lr=0.05,
+    optimizer = torch.optim.SGD(params, lr=0.005,
                                 momentum=0.9, weight_decay=0.0005)
 
     # and a learning rate scheduler which decreases the learning rate by
